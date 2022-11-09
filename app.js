592c93dab35a4f8d9b2d96720ec040a374b3ce0b
Sahil Halgekar <sahilhalgekar07@gmail.com>
2022-11-09 20:09:01 +0530

    changes to app.js

--- app.js
+++ app.js
@@ -2,11 +2,7 @@ const express=require("express");
 const cors = require("cors");
 const dotenv=require("dotenv");
 const mongoose=require("mongoose");
-const authRoutes = require('./auth.routes');
-const groupRoutes = require('./group.routes');
-const expenseRoutes = require('./expense.routes');
-const userRoutes = require('./user.routes');
-const commentRoutes = require('./comment.routes');
+
 const cookieParser = require("cookie-parser");
 
 const app=express();
@@ -29,23 +25,8 @@ const connectDB = async function () {
 
 connectDB();
 
-
-// Auth routes
-router.use('/auth', authRoutes);
-
-
-
-// Group routes
-app.use('/api/groups', groupRoutes);
-
-// Expense routes
-app.use('/api/groups/:groupId/expenses', expenseRoutes);
-
-// Comment routes
-app.use('/api/groups/:groupId/expenses/:expenseId/comments', commentRoutes);
-
-// User routes
-app.use('/api/userInfo', userRoutes);
+const allRoutes = require('./routes/index.routes');
+app.use('/api', allRoutes);
 
 require('./error-handling')(app);
 app.set("trust proxy", 1);
@@ -53,6 +34,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 
-app.listen(process.env.PORT|| 5000,()=>{
+app.listen(process.env.PORT || 5000,()=>{
     console.log("backend is running");
 })
